5988338e492d1894ac59c50fba8ee7c0a62bb67d
Diego López <lopez.diego@cetys.edu.mx>
2024-09-11 19:20:15 -0700

    Removed node_modules and .vercel from tracking

--- website/node_modules/@egjs/react-flicking/src/react-flicking/ViewportSlot.tsx
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2015 NAVER Corp.
- * egjs projects are licensed under the MIT license
- */
-import React, { ReactNode } from "react";
-
-const ViewportSlot = React.memo((props: { children?: ReactNode }) => <>{props.children}</>);
-
-export default ViewportSlot;
